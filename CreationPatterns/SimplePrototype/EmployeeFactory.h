78094acd59db5890b137f35d4c18acab9ef64d34
Clinton van Ry <clinton.vanry@gmail.com>
2020-01-05 11:26:24 +0000

    Prototype Factory

---
+++ CreationPatterns/SimplePrototype/EmployeeFactory.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "Contact.h"
+
+class EmployeeFactory
+{
+
+public:
+    static std::unique_ptr<Contact> NewMainOfficeEmployee(const std::string& name, int suite)
+    {
+        //static Contact p{ "", new Address{ "123 East Dr", "London", 0 } };
+        return new_employee(name, suite, main);
+    }
+
+    static std::unique_ptr<Contact> NewAuxOfficeEmployee(const std::string& name, int suite)
+    {
+        return new_employee(name, suite, aux);
+    }
+	
+private:
+
+    inline static Contact main {"",new Address{"123 East Dr", "London", 0}};
+    inline static Contact aux{ "",new Address{"123 East Dr", "London", 0} };
+	
+	static std::unique_ptr<Contact> new_employee(const std::string& name, int suite, const Contact& prototype)
+	{
+		auto result = std::make_unique<Contact>(prototype);
+		result->setName(name);
+		result->setSuite(suite);
+		return result;
+	}
+};
+
--- CreationPatterns/SimplePrototype/SimplePrototype.cpp
+++ CreationPatterns/SimplePrototype/SimplePrototype.cpp
@@ -1,19 +1,23 @@
 
 #include <iostream>
 #include "Contact.h"
+#include "EmployeeFactory.h"
 
 int main()
 {
     std::cout << "Hello World!\n";
 
-	Contact john{"John Doe", new Address{"123 East Dr","London",10}};
-	//Contact jane{ "Jane Smith", new Address{"123 East Dr","London",20} };
-    auto jane = john;
-	jane.setName("Jane Smith");
-	jane.setSuite(15);
+	//Contact john{"John Doe", new Address{"123 East Dr","London",10}};
+	////Contact jane{ "Jane Smith", new Address{"123 East Dr","London",20} };
+ //   auto jane = john;
+	//jane.setName("Jane Smith");
+	//jane.setSuite(15);
+
+	auto john = EmployeeFactory::NewMainOfficeEmployee("John Doe", 10);
+	auto jane = EmployeeFactory::NewAuxOfficeEmployee("Jane Smith", 15);
 
 	
-	std::cout << john << std::endl;
+	std::cout << *john << std::endl;
 
-	std::cout << jane << std::endl;
+	std::cout << *jane << std::endl;
 }
